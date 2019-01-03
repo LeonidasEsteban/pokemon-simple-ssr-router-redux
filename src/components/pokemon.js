8bb71bbb77dcc5c080f55044c097a0fda62b53b6
Leonidas Esteban <leonidasesteban@gmail.com>
2019-01-03 02:05:06 -0600

    add moves and sprites to pokemon/id

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const PokemonStyled = styled.div`
+  border: 1px solid red;
+  .name {
 
-const Pokemon = ({ name, sprites }) => (
-  <div>
-    <h2>Nombre: {name} x</h2>
+  }
+`
+
+const Pokemon = ({ name, sprites, moves }) => (
+  <PokemonStyled>
+    <h2 className="name">Nombre: {name}</h2>
     <img src={sprites.front_default} alt="" />
-  </div>
+    <img src={sprites.back_default} alt="" />
+    <img src={sprites.front_shiny} alt="" />
+    <img src={sprites.back_shiny} alt="" />
+    <ol>
+      {moves.map(({ move })=>(
+        <li>{move.name}</li>
+      ))}
+    </ol>
+  </PokemonStyled>
 )
 
 export default Pokemon
